da147a00305bf73b6a1eb3201606118c90a10fba
fred <f@vdberg.us>
2024-11-08 12:51:14 +0100

    added papers

---
+++ web.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+df = pandas.read_csv("docs/docs.csv", sep=";")
+
+sections = [("adsl","aDSL publications"),
+            ("idsl","iDSL publications"),
+            ("thesis","Theses"),
+            ("other","Other publications"),]
+
+for section_id, section_name in sections:
+    st.title(section_name)
+    for index,row in df.iterrows():
+        if section_id == row["category"]:
+            st.write(f"[{row['name']}](docs/{row["filename"]}.pdf)",
+                     "<br>",row["authors"],
+                     ",",row["source"],unsafe_allow_html=True)
+
